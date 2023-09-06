52cc2f8029ff24883ef7e3f67d54a85beb283d6f
Saxar88 <v.sasaran@protonmail.com>
2023-09-06 08:14:16 +0300

    fix 'Dynamic server usage' error

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,38 @@
-import getCurrentUser from './actions/getCurrentUser';
-import getListings, { IListingsParams } from './actions/getListings';
-import Container from './components/Container';
-import EmptyState from './components/EmptyState';
-import ListingCard from './components/listings/ListingCard';
+export const dynamic = "force-dynamic";
+
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings, { IListingsParams } from "./actions/getListings";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 interface HomeProps {
-    searchParams: IListingsParams;
+	searchParams: IListingsParams;
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-    const listings = await getListings(searchParams);
-    const currentUser = await getCurrentUser();
+	const listings = await getListings(searchParams);
+	const currentUser = await getCurrentUser();
 
-    if (listings.length === 0) {
-        return <EmptyState showReset />;
-    }
+	if (listings.length === 0) {
+		return <EmptyState showReset />;
+	}
 
-    return (
-        <Container>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 pt-24">
-                {listings.map((listing: any) => {
-                    return (
-                        <ListingCard
-                            key={listing.id}
-                            data={listing}
-                            currentUser={currentUser}
-                        />
-                    );
-                })}
-            </div>
-        </Container>
-    );
+	return (
+		<Container>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 pt-24">
+				{listings.map((listing: any) => {
+					return (
+						<ListingCard
+							key={listing.id}
+							data={listing}
+							currentUser={currentUser}
+						/>
+					);
+				})}
+			</div>
+		</Container>
+	);
 };
 
 export default Home;
