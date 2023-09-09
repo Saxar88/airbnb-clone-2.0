5dc677398c72e3637ad61d881a7415a8ea8928de
Saxar88 <v.sasaran88@gmail.com>
2023-09-09 15:07:02 +0300

    add geckodriver auto-install to tests

--- tests/log_out_test.py
+++ tests/log_out_test.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(
+    service=FirefoxService(GeckoDriverManager().install()))
 
 driver.get("https://rent-site-clone-2-0.vercel.app/")
 
