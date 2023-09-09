e29ed023a6934e71daa6306c1ec43dba3ecd7eba
Saxar88 <v.sasaran88@gmail.com>
2023-09-09 18:45:01 +0300

    fix geckodriver issue

--- tests/log_in_test.py
+++ tests/log_in_test.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Firefox(
-    service=FirefoxService(GeckoDriverManager().install()))
+driver = webdriver.Firefox()
 
 driver.get("https://rent-site-clone-2-0.vercel.app/")
 
