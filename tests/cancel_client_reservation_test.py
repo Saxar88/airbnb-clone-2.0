d9d1a642ba650d60ac97444c7349ec39e80c0372
Saxar88 <v.sasaran@protonmail.com>
2023-09-10 22:26:34 +0300

    add Python test for cencelling client reservation

---
+++ tests/cancel_client_reservation_test.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+browsers = ["chrome", "firefox"]
+
+for browser in browsers:
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        options = Options()
+        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        driver = webdriver.Firefox(options=options)
+
+driver.get("https://rent-site-clone-2-0.vercel.app/")
+
+try:
+    wait = WebDriverWait(driver, 10)
+    user_menu = wait.until(
+        EC.presence_of_element_located((By.ID, 'user-menu')))
+    user_menu.click()
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, "//*[text()='Log in']"))).click()
+    driver.find_element(
+        "id", "email").send_keys("test1@test.com")
+    driver.find_element(
+        "id", "password").send_keys("test123")
+    driver.find_element(
+        "xpath", "//*[text()='Continue']").click()
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, "//*[text()='Logged in successfully!']")))
+    wait.until(EC.invisibility_of_element_located(
+        (By.ID, "modal")))
+
+    time.sleep(0.5)
+    driver.find_element(
+        "xpath", "//*[text()='My reservations']").click()
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, "/html[1]/body[1]/div[3]/div[1]/div[2]/div[1]/div[1]/button[1]"))).click()
+    wait.until(EC.invisibility_of_element_located(
+        (By.XPATH, "//*[text()='Reservation cancelled']")))
+
+finally:
+    driver.quit()
